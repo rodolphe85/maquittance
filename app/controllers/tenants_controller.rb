7b3890f9a0c5e9a07b770c3772283464f87d52b3
Rodolphe Geant <rodolphe@rgeant.name>
2017-12-15 13:49:11 +0400

    Tenants#index view create with edit and delete actions

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -4,28 +4,47 @@ class TenantsController < ApplicationController
   end
 
   def new
-    @proper = Landlord.find(params[:landlord_id])
-    @property = @landlord.properties.build
+    @tenant = Tenant.new
   end
 
   def create
+     @tenant = Tenant.new(tenant_params)
+
+    if @tenant.save
+      redirect_to tenants_url
+    else
+      render 'new'
+    end
   end
 
   def show
+    @tenant = Tenant.find(params[:id])
   end
 
   def edit
+    @tenant = Tenant.find(params[:id])
   end
 
   def update
+    @tenant = Tenant.find(params[:id])
+
+    if @tenant.update_attributes(tenant_params)
+      flash[:success] = "locataire mis à jour!"
+      redirect_to tenants_url
+    else
+      render :edit
+    end
   end
 
   def destroy
+    Tenant.find(params[:id]).destroy
+    flash[:success] = "Tenant deleted"
+    redirect_to tenants_url
   end
 
   private
 
   def tenant_params
-    params.require(:tenant).permit(:name, :street, :city, :zip, :email, :phone)
+    params.require(:tenant).permit(:name, :street, :city, :zip, :email, :phone, :property_id)
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,8 @@ class Property < ApplicationRecord
   belongs_to :landlord
   has_many :tenants, dependent: :destroy
 
+  def receivable
+    (self.rent.to_i + self.charges.to_i)
+  end
+
 end
