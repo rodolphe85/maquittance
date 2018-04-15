9033db5e552be13f20d406d802d6b4a3a0aeb033
Rodolphe Geant <rodolphe@rgeant.name>
2018-04-15 16:22:23 +0400

    finish pdf rendering

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -18,4 +18,7 @@ WickedPdf.config = {
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
   # layout: 'pdf.html',
+  page_size: 'A4',
+  formats: :html, encoding: 'utf8'
+
 }
